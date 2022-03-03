bbf7bc9f9c097b6dcd9f91974d9031d531ac9c67
Silverio O Paula <contato@silverio.eti.br>
2022-03-03 19:21:01 -0300

    refactor: metodos e atributos privados

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,26 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
 }
+
+class ContaCorrente {
+    agencia;
+    _saldo = 0;    
+
+    sacar(valor) {
+        if(this._saldo >= valor) {
+            this._saldo -= valor;
+        }
+    }
+
+    depositar(valor) {
+        if(valor > 0) {
+            this._saldo += valor;
+        }
+    }
+}
+
+const cliente1 = new ContaCorrente();
+cliente1.depositar(100);
+cliente1.sacar(50);
+console.log(cliente1);
