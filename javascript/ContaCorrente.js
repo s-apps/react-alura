b3df9cc0ebd6bee297481b6833dc7c807cda8b74
Silverio O Paula <contato@silverio.eti.br>
2022-03-04 18:47:34 -0300

    feat: acessores get set

--- javascript/ContaCorrente.js
+++ javascript/ContaCorrente.js
@@ -1,7 +1,22 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
     agencia;
-    cliente;
-    _saldo = 0;    
+    _cliente;
+    _saldo = 0;  
+    
+    set cliente(cliente) {
+        if(cliente instanceof Cliente)
+            this._cliente = cliente;
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
 
     sacar(valor) {
         if(this._saldo >= valor) {
--- javascript/index.js
+++ javascript/index.js
@@ -2,30 +2,31 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";//precisa passar a extensão do arquivo
 
+const conta1 = new ContaCorrente();
+conta1.agencia = "000";
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Teste 1";
 cliente1.cpf = "000000000-00";
+conta1.cliente = cliente1;
 
+const conta2 = new ContaCorrente();
+conta2.agencia = "111"; 
+const cliente2 = new Cliente();
 cliente2.nome = "Teste 2";
 cliente2.cpf = "111111111-11";
+conta2.cliente = cliente2;
 
-const contaCorrente1 = new ContaCorrente();
-contaCorrente1.agencia = "000";
-contaCorrente1.cliente = cliente1;
 
-const contaCorrente2 = new ContaCorrente();
-contaCorrente2.agencia = "111"; 
-contaCorrente2.cliente = cliente2;
+conta1.depositar(100);
+conta2.depositar(50);
 
-contaCorrente1.depositar(100);
-contaCorrente2.depositar(50);
+console.log("conta corrente 1", conta1);
+console.log("conta corrente 2", conta2);
 
-console.log("conta corrente 1", contaCorrente1);
-console.log("conta corrente 2", contaCorrente2);
+conta1.transferir(50, conta2);
 
-contaCorrente1.transferir(50, contaCorrente2);
+console.log("conta corrente 1", conta1);
+console.log("conta corrente 2", conta2);
 
-console.log("conta corrente 1", contaCorrente1);
-console.log("conta corrente 2", contaCorrente2);
+console.log("cliente 1", conta1.cliente);
+console.log("cliente 2", conta2.cliente);
