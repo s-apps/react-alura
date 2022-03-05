75db7bfed4010b6af6769517b888075e8d2b8569
Silverio O Paula <contato@silverio.eti.br>
2022-03-05 14:54:24 -0300

    feat: polimorfismo

---
+++ javascript/Funcionario/Funcionario.js
@@ -0,0 +1,19 @@
+export class Funcionario {
+
+    constructor() {
+        this._nome;
+        this._salario;
+        this._cpf;
+        this._bonificacao = 1;
+        this._senha;
+    }
+
+    get senha() {
+        return this._senha;
+    }
+
+    cadastrarSenha(senha) {
+        this._senha = senha;
+    }
+
+}
--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,13 @@
 
-import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";//precisa passar a extensão do arquivo
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Teste 1", "000000000-00");
-const conta1 = new ContaCorrente(cliente1, "000");
+const diretor = new Diretor("Teste", 11000, "000000000-00");
+const gerente = new Gerente("Teste G", 1200, "111111111-11");
 
-const cliente2 = new Cliente("Teste 2", "111111111-11");
-const conta2 = new ContaPoupanca(0, cliente2, "111");
+diretor.cadastrarSenha("123456");
 
+const estaLogado = SistemaAutenticacao.login(diretor, "123456" );
 
-conta1.depositar(100);
-conta2.depositar(50);
-
-console.log("conta corrente 1", conta1);
-console.log("conta corrente 2", conta2);
-
-conta1.transferir(50, conta2);
-
-console.log("conta corrente 1", conta1);
-console.log("conta corrente 2", conta2);
-
-console.log("Número de Contas Corrente", ContaCorrente.numeroDeContas);
-console.log("Número de Contas Poupança", ContaCorrente.numeroDeContas);
+console.log(estaLogado)
