9e24528fa751ab62e3b5aa48d0b6df28563b4218
Silverio O Paula <contato@silverio.eti.br>
2022-03-05 14:58:14 -0300

    refactor: polimorfismo

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,10 @@ const diretor = new Diretor("Teste", 11000, "000000000-00");
 const gerente = new Gerente("Teste G", 1200, "111111111-11");
 
 diretor.cadastrarSenha("123456");
+gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456" );
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "1234567" );
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
 
-console.log(estaLogado)
+console.log("diretor está logado ", diretorEstaLogado);
+console.log("gerente está logado ", gerenteEstaLogado);
