5a8ef9a6b81fcb82e8164431fb976f828a1e6295
Silverio O Paula <contato@silverio.eti.br>
2022-03-06 10:04:41 -0300

    refator: componetizando o sistema

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,7 +1,16 @@
-function App() {
-  return (
-    <h1>First app</h1>
-  );
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import  FormularioCadastro  from "./components/FormularioCadastro";
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
